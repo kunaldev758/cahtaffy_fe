2bfd26a0bdbdb7c986775623b1092060cfabb81a
KhailendraActivantDeveloper <khailendraprasad.deskmoz@gmail.com>
2025-07-08 13:58:16 +0530

    middleware changes

--- middleware.ts
+++ middleware.ts
@@ -30,7 +30,7 @@ if (cookies().has('token') && currentUserrole === 'agent') {
   }
 }
 
-  if (!cookies().has('token') && !pathname.startsWith('/login')) {
+  if (!cookies().has('token') && !pathname.startsWith('/login') && !pathname.startsWith('/agent-login')) {
     return NextResponse.redirect(baseUrl + 'login');
   }
 }
