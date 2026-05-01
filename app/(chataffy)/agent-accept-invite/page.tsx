0133cb14e6de2f2c0d493d6a6c7ade174f433e14
Mohammad Asjad <mohammadasjad.deskmoz@gmail.com>
2026-05-01 13:03:52 +0530

    fix inbox page rerender issue

--- app/(chataffy)/agent-accept-invite/page.tsx
+++ app/(chataffy)/agent-accept-invite/page.tsx
@@ -1,37 +1,10 @@
-"use client"
-import { agentAcceptInviteVerify } from "@/app/_api/dashboard/action"
-import { useRouter, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
-import { toast } from "react-toastify"
+import { Suspense } from "react";
+import AgentAcceptInviteContent from "./AgentAcceptInviteContent";
 
-export default function AgentAcceptInvite() {
-    // Add auth check here if needed
-
-    const params = useSearchParams()
-    const router=useRouter()
-
-    const token = params?.get('token')
-    console.log(token)
-
-    const veriFyToken = async () => {
-        try {
-            const res = await agentAcceptInviteVerify(token)
-            console.log(res)
-            if(res.message){
-                toast.success(res.message)
-                setTimeout(()=>{
-                    router.push("/agent-login")
-                },2000)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    useEffect(() => {
-        veriFyToken()
-
-
-    }, [token])
-    return <h1>Welcome! You are being redirected to login Page</h1>
-
+export default function AgentAcceptInvitePage() {
+  return (
+    <Suspense fallback={null}>
+      <AgentAcceptInviteContent />
+    </Suspense>
+  );
 }
