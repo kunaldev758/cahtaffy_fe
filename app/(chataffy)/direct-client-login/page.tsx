8cea9e621677453515ad87eb12424df5acaa46b7
harshita <harshitakumawat.deskmoz@gmail.com>
2026-05-07 15:48:30 +0530

    page

---
+++ app/(chataffy)/direct-client-login/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Direct Client Login",
+};
+
+export default function DirectClientLoginPage() {
+  return (
+    <><h1>
+      HIIII</h1></>
+  );
+}
