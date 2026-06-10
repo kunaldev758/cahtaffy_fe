900dd103f8201112c02090979bd72d36b2489cd6
KhailendraActivantDeveloper <khailendraprasad.deskmoz@gmail.com>
2026-06-10 12:20:55 +0530

    google auth check 1

--- next.config.js
+++ next.config.js
@@ -43,15 +43,15 @@ const nextConfig = {
   },
   async headers() {
     return [
-      {
-        source: "/(.*)",
-        headers: [
-          // {
-          //   key: "Cross-Origin-Opener-Policy",
-          //   value: "same-origin-allow-popups",
-          // },
-        ],
-      },
+      // {
+      //   source: "/(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cross-Origin-Opener-Policy",
+      //       value: "same-origin-allow-popups",
+      //     },
+      //   ],
+      // },
       {
         source: "/openai/widget/:path*",
         headers: [
